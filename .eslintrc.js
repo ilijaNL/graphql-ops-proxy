bcca0928cc36bf9d0afcd8792438c7b50ea4c351
ilijaNL <ilija.lisenkov@gmail.com>
2023-04-13 11:36:29 +0200

    Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
 module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: [ "prettier"]
-};
+  "parser": "@typescript-eslint/parser",
+  "extends": ["plugin:@typescript-eslint/recommended", "prettier"],
+  "parserOptions": { "ecmaVersion": 2018, "sourceType": "module" },
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "rules": {
+    "no-console": 2,
+    "@typescript-eslint/no-inferrable-types": 0,
+    // should remove this in the end
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/ban-types": 0
+  }
+}
